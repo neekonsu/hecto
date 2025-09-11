28f83ad7a3e345b282347ea3281d273900bd4b3b
Neekon Saadat <neekonsu@gmail.com>
2025-09-10 22:53:26 -0700

    initial commit of hecto up to half way through chapter 2, prior to 'Display Keypresses' section

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::io::{self, Read};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+
+fn main() {
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c=='q' {
+            disable_raw_mode().unwrap();
+            break;
+        }
+    }
+}
